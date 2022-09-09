89b9916e1db6dcfb2ed56a8a7a0d36074a28582c
scully06 <moroboshikirari@outlook.jp>
2022-09-09 21:56:14 +0900

    動作のさらなる高速化とバグの修正

--- 画像抽出.py
+++ 画像抽出.py
@@ -6,6 +6,7 @@ from PIL import Image
 from fake_useragent import UserAgent
 import urllib.request
 from urllib.request import urlopen
+from concurrent import futures
 
 @retry(wait=wait_fixed(3))
 def get_URL(url, file_path):
@@ -19,12 +20,14 @@ def get_URL(url, file_path):
     html = tree.getroot()
     for image in html.cssselect("img"):
         url_list.append(image.get("src"))
-    for link in url_list:
+    def get_img(link):
         try:
+            a = 1
             urlData = urlopen(link).read()
             filename = os.path.basename(link)
             with open(file_path + "/" + filename, "wb") as g:
                 g.write(urlData)
+                print("1")
             try:
                 with Image.open(file_path + "/" + filename) as r:
                     width , height = r.size
@@ -32,20 +35,23 @@ def get_URL(url, file_path):
                     try:
                         im.verify()
                     except Exception as q:
-                        print("1")
+                        #print("1")
                         print(q)
                         a = 0
             except Exception as j:
                 print(j)
-                a = 0
+                #a = 0
             if width <= 500 or height <= 500:
                 a = 0
-            else:
-                continue
             if a == 0:
+                #print("2")
                 os.remove(file_path + "/" + filename)
         except Exception as e:
             if os.path.exists(file_path + "/" + filename) == True:
                 os.remove(file_path + "/" + filename)
             print(e)
-            continue
+    try:
+        with futures.ThreadPoolExecutor() as executor:
+            executor.map(get_img, url_list)
+    except Exception as j:
+        print(j)
